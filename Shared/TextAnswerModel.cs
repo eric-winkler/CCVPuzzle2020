5893030f98d6d4b047760f62eadd5e91adbf634d
Eric Winkler <eric@winkler.net.au>
2020-08-14 11:19:57 +0800

    WIP

---
+++ Shared/TextAnswerModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PuzzlePortal.Shared
+{
+    public class TextAnswerModel
+    {
+        [Required]
+        [StringLength(255, ErrorMessage = "Answer is too long")]
+        public string Answer { get; set; }
+    }
+}
