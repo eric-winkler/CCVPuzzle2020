241c10ee23fec047dc19760d90f0249eeee3182b
Eric Winkler <eric@winkler.net.au>
2020-08-20 12:24:55 +0800

    Speculative future puzzle ideas

--- Shared/Puzzle.cs
+++ Shared/Puzzle.cs
@@ -8,6 +8,14 @@ namespace PuzzlePortal.Shared
     {
         public static readonly Puzzle Seafood = new Puzzle { Name = nameof(Seafood), Id = new Guid("d2f541a3-9d9a-4e85-9d98-a0049fa60acf") };
         public static readonly Puzzle SimeonWheelock = new Puzzle { Name = nameof(SimeonWheelock), Id = new Guid("1ef4df75-ecbe-4d30-b06e-dce1c7678c61") };
+        
+        // TODO:
+        public static readonly Puzzle Skewers = new Puzzle { Name = nameof(Skewers), Id = new Guid("8cfcdc16-fc45-472b-8a9f-a10b995e4248") };
+        public static readonly Puzzle CuminsLore = new Puzzle { Name = nameof(CuminsLore), Id = new Guid("fc4acc00-04c0-4fb5-8ea8-129dbe24ca1f") };
+        public static readonly Puzzle Codenames = new Puzzle { Name = nameof(Codenames), Id = new Guid("8ddcc5ef-f2b7-4d88-ab02-38e311597cf5") };
+        // launchdates?
+        // glcodes vs gldescription?
+
 
         public static Dictionary<Guid, Puzzle> ById { get; } = new Dictionary<Guid, Puzzle>
         {
