fbec4f50b43c3531f9b9eaeb4ba4329e8d235d60
Eric Winkler <eric@winkler.net.au>
2020-08-25 23:47:52 +0800

    WIP: CuminsLore

---
+++ Server/Controllers/CuminsLoreController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using PuzzlePortal.Shared;
+using PuzzlePortal.Server.Domain;
+
+namespace PuzzlePortal.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/{action}")]
+    [AuthorizeScoreSheet(currentPuzzle: Puzzle.GuidStrings.Skewers)]
+    public class SkewersController : QuestionControllerBase
+    {
+        private static readonly string[] RealSkus = new[] { "Dragon", "Golden Cane", "Rattle", "Handcuffs", "Batteries", "Astroturf", "Cut Padlock", "Broom Head", "Kitchen Sink" };
+        private static readonly string[] FakeSkus = new[] { "USB Cable", "Lightsabre", "Armour", "Saucepan", "Time Machine" };
+        private readonly IQuizMaster _quizMaster;
+
+        public SkewersController(IQuizMaster quizMaster)
+        {
+            _quizMaster = quizMaster;
+        }
+
+        [HttpPost]
+        public ActionResult<ScoreSheetModel> Answer(TrueFalseListAnswerModel model)
+        {
+            if(!model.Answer.Any()
+                || !model.Answer.Where(a => a.Value).Select(a => a.Key).All(m => RealSkus.Contains(m))
+                || !model.Answer.Where(a => !a.Value).Select(a => a.Key).All(m => FakeSkus.Contains(m)))
+            {
+                return BadRequest();
+            }
+
+            return _quizMaster.CompletePuzzle(ScoreSheet).ToModel();
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Question()
+        {
+            return RealSkus.Concat(FakeSkus)
+                .OrderBy(i => Guid.NewGuid())
+                .Take(5)
+                .ToArray();
+        }
+    }
+}
--- Shared/Puzzle.cs
+++ Shared/Puzzle.cs
@@ -11,14 +11,15 @@ namespace PuzzlePortal.Shared
             public const string SimeonWheelock = "1ef4df75-ecbe-4d30-b06e-dce1c7678c61";
             public const string Seafood = "d2f541a3-9d9a-4e85-9d98-a0049fa60acf";
             public const string Skewers = "8cfcdc16-fc45-472b-8a9f-a10b995e4248";
+            public const string CuminsLore = "fc4acc00-04c0-4fb5-8ea8-129dbe24ca1f";
         }
 
         private static readonly Puzzle Seafood = new Puzzle { Name = nameof(Seafood), Id = new Guid(GuidStrings.Seafood) };
         private static readonly Puzzle SimeonWheelock = new Puzzle { Name = nameof(SimeonWheelock), Id = new Guid(GuidStrings.SimeonWheelock) };
         private static readonly Puzzle Skewers = new Puzzle { Name = nameof(Skewers), Id = new Guid(GuidStrings.Skewers) };
+        private static readonly Puzzle CuminsLore = new Puzzle { Name = nameof(CuminsLore), Id = new Guid(GuidStrings.CuminsLore) };
 
         // TODO:
-        private static readonly Puzzle CuminsLore = new Puzzle { Name = nameof(CuminsLore), Id = new Guid("fc4acc00-04c0-4fb5-8ea8-129dbe24ca1f") };
         private static readonly Puzzle Codenames = new Puzzle { Name = nameof(Codenames), Id = new Guid("8ddcc5ef-f2b7-4d88-ab02-38e311597cf5") };
         // launchdates?
         // glcodes vs gldescription?
@@ -28,7 +29,8 @@ namespace PuzzlePortal.Shared
         {
             { Seafood.Id, Seafood },
             { SimeonWheelock.Id, SimeonWheelock },
-            { Skewers.Id, Skewers }
+            { Skewers.Id, Skewers },
+            { CuminsLore.Id, CuminsLore},
         };
         public static Guid[] Ids { get; } = ById.Keys.ToArray();
 
